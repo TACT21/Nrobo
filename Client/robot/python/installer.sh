e79850d2b1632665cd7a778d69b2ed459c30e00c
TACT21 <takumokumoku@gmail.com>
2024-02-17 08:25:23 +0900

    Remove legacy network manager

--- Client/robot/python/installer.sh
+++
@@ -1,3 +0,0 @@
-apt install hostapd dnsmasq
-apt install -y python3 python3-pip
-pip install Adafruit-BMP
--- Client/robot/python/setup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import os
-
-subprocess.run([os.path.dirname(os.path.abspath(__file__)) + "/installer.sh",""], shell=True)
-
-https://www.mikan-tech.net/entry/raspi-wifi-ap
